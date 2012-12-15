efde085762c7a9fe0a8cfcf5aff3784e6ba332c9
Grant Kurtz <th3guys2@gmail.com>
2012-12-15 14:33:09 -0500

    Fixed readme to GitHub knows to parse it as markdown.  Inlcuded all the files as provided by the instructor for this assignment.

---
+++ README.md
@@ -0,0 +1,23 @@
+Polyfill Algorithm
+------------------
+
+A Polyfill Algorithm for Computer Graphics I.
+
+Building
+--------
+javac src/*.java
+
+Executing
+---------
+java FillTest
+
+Dependencies
+------------
+
+* Java 1.6.36
+
+These files were provided for this assignment:
+* FillTest.java (renamed from fillTest.java)
+* Rasterizer.java
+* simpleCanvas.java
+This file was modified by IntelliJ IDEA 12.0 for binding GitHub repository
---
+++ src/Rasterizer.java
@@ -0,0 +1,45 @@
+//
+//  Rasterizer.java
+//  
+//
+//  Created by Joe Geigel on 1/21/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+/**
+ *
+ * This is a class that performas rasterization algorithms
+ *
+ */
+
+public class Rasterizer {
+
+	/**
+	 * number of scanlines
+	 */
+	int n_scanlines;
+
+	/**
+	 * Constructor
+	 *
+	 * @param n - number of scanlines
+	 */
+	Rasterizer(int n) {
+		n_scanlines = n;
+	}
+
+	/**
+	 * Draw a filled polygon in the simpleCanvas C.
+	 * <p/>
+	 * The polygon has n distinct vertices. The
+	 * coordinates of the vertices making up the polygon are stored in the
+	 * x and y arrays.  The ith vertex will have coordinate  (x[i], y[i])
+	 * <p/>
+	 * You are to add the implementation here using only calls
+	 * to C.setPixel()
+	 */
+	public void drawPolygon(int n, int x[], int y[], simpleCanvas C) {
+		// YOUR IMPLEMENTATION GOES HERE
+	}
+
+}
